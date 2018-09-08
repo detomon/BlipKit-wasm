56fd5b4677e543f2d7b67b0efb9386c69f3ce5dd
detomon <simon@monoxid.ch>
2018-09-08 18:19:31 +0200

    Update files

--- assets/js/controller.js
+++ assets/js/controller.js
@@ -51,15 +51,22 @@ class BliplayController {
 		return this._heapSubarray(array, this.HEAP32, offset, length);
 	}
 
+	heapInt64Array(array, offset, length) {
+		return this._heapSubarray(array, this.HEAP64, offset, length);
+	}
+
 	heapFloat32Array(array, offset, length) {
 		return this._heapSubarray(array, this.HEAPF32, offset, length);
+	}
+
+	heapFloat64Array(array, offset, length) {
+		return this._heapSubarray(array, this.HEAPF64, offset, length);
 	}
 
 	setHeapInt8Array(array, data, offset) {
 		offset = offset || 0;
 		const memory = this.HEAP8;
 		const offsetStart = array / memory.BYTES_PER_ELEMENT + offset;
-		//const offsetEnd = offsetStart + length;
 
 		return memory.set(data, offsetStart);
 	}
@@ -119,7 +126,7 @@ class BliplayController {
 	}
 
 	locateFile(file) {
-		return 'assets/' + file;
+		return 'assets/js/' + file;
 	}
 
 	_filePutContents(path, data) {
@@ -128,7 +135,7 @@ class BliplayController {
 
 	_loadSamples(paths) {
 		let fetches = paths.map((path) => {
-			console.log('Loading sample', path);
+			console.debug('Loading sample', path);
 
 			return fetch('assets/sound/' + path).then((result) => {
 				return result.arrayBuffer();
