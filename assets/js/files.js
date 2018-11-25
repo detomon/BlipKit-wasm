4e08d6274dcffc42c22f78e4e1e554669f885c8b
detomon <simon@monoxid.ch>
2018-11-25 11:54:21 +0100

    Update assets

--- assets/js/files.js
+++ assets/js/files.js
@@ -1,64 +1,14 @@
-const files = [{
-	title: 'Tutorial',
-	subdir: 'tutorial/',
-	files: [{
-		title: 'Power On',
-		path: 'power-on.blip',
-	}, {
-		title: 'Waveforms',
-		path: 'waveforms.blip',
-	}, {
-		title: 'Instruments',
-		path: 'instruments.blip',
-	}, {
-		title: 'Samples',
-		path: 'samples.blip',
-	}],
-}, {
-	title: 'Loops',
-	subdir: 'loops/',
-	files: [{
-		title: 'Loop #1',
-		path: 'loop-1.blip',
-	}, {
-		title: 'Loop #2',
-		path: 'loop-2.blip',
-	}],
-}, {
-	title: 'Tracks',
-	files: [{
-		title: 'Don\'t Eat Flashcards',
-		path: 'dont-eat-flashcards.blip',
-	}, {
-		title: 'Short-Fused Bombs',
-		path: 'short-fused-bombs.blip',
-	}, {
-		title: 'Ghost Bouncer',
-		path: 'ghost-bouncer.blip',
-	}, {
-		title: 'Hyperion Star Racer',
-		path: 'hyperion-star-racer.blip',
-	}, {
-		title: 'Killer Squid',
-		path: 'killer-squid.blip',
-	}, {
-		title: 'WYSIWYG',
-		path: 'wysiwyg.blip',
-	}, {
-		title: 'Cave XII',
-		path: 'cave-xii.blip',
-	}, {
-		title: 'Monster Carousel',
-		path: 'monster-carousel.blip',
-	}, {
-		title: 'Dirt Planet',
-		path: 'dirt-planet.blip',
-	}],
-}];
-
 app.addInit(function () {
 
-function initFileSelect(select) {
+function fetchFileIndex() {
+	const url = app.dirs.files + 'index.json';
+
+	return fetch(url).then((response) => {
+		return response.json();
+	});
+}
+
+function initFileSelect(select, files) {
 	select.addEventListener('change', (e) => {
 		app.changeFile();
 		app.stopAction();
@@ -83,11 +33,17 @@ function initFileSelect(select) {
 
 app.fileSelect = document.querySelector('#file-select');
 
-initFileSelect(app.fileSelect);
+app.fileIndexPromise = fetchFileIndex().then((files) => {
+	initFileSelect(app.fileSelect, files);
+});
+
+app.addLoadPromise(app.fileIndexPromise);
 
 function selectedOption(select) {
 	return select.options[select.selectedIndex];
 }
+
+let files = {};
 
 app.changeFile = function () {
 	const option = selectedOption(app.fileSelect);
@@ -95,10 +51,15 @@ app.changeFile = function () {
 	const data = option.data;
 	let dataPromise;
 
-	if (path) {
+	if (files[path]) {
+		dataPromise = files[path];
+	}
+	else if (path) {
 		dataPromise = fetch(path).then((response) => {
 			return response.text();
 		});
+
+		files[path] = dataPromise;
 	}
 	else if (data) {
 		dataPromise = Promise.resolve(data);
